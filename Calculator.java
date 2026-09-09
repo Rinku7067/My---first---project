93553b2b67c13807311be50c8c9c7e05a0e90a5b
Rinku7067 <thakur7067rinku@gmail.com>
2026-09-10 02:21:04 +0530

    Fix concatenation in sum output

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,6 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println("Sum = "+ (a+b));
+        System.out.println("Sum = " + (a+b));
     }
 }
